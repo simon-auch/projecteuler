58112d1dcb62df83ce05b178f1206c2698bfd32d
Simon Auch <auchsimon@googlemail.com>
2020-10-13 22:38:31 +0200

    solution for 0029

---
+++ src/bin/problem_0029.rs
@@ -0,0 +1,26 @@
+#[path = "../primes.rs"]
+mod primes;
+
+use core::ops::Range;
+
+fn main() {
+    dbg!(solve(2..6,2..6));
+    dbg!(solve(2..101,2..101));
+}
+
+fn solve(a: Range<usize>, b: Range<usize>) -> usize {
+    let mut acc: Vec<Vec<(usize,usize)>> = Vec::new();
+    for a in a{
+        let factorization = primes::factorize_count(a);
+        for b in b.clone() {
+            acc.push(
+                factorization.iter().map(|x| 
+                    (x.0, x.1*b)
+                ).collect()
+            );
+        }
+    }
+    acc.sort();
+    acc.dedup();
+    acc.len()
+}
