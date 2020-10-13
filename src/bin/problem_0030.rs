4ead0738e835fc2a680c389aaeb5a55a7fc5a4fa
Simon Auch <auchsimon@googlemail.com>
2020-10-13 23:48:44 +0200

    solution for 0030

---
+++ src/bin/problem_0030.rs
@@ -0,0 +1,33 @@
+#[path = "../helper.rs"]
+mod helper;
+
+fn main() {
+    dbg!(solve(4));
+    dbg!(solve(5));
+}
+fn solve(power: usize) -> usize {
+    let mut sum = 0;
+    //first find the upper limit of digits
+    let powers: Vec<usize> = [0usize,1,2,3,4,5,6,7,8,9].iter().map(|d| (*d).pow(power as u32)).collect();
+    let max = {
+        let mut a = 0;
+        let mut b = 0;
+        while a >= b {
+            a += powers[9];
+            b *= 10;
+            b += 9;
+        }
+        a
+    };
+    //dbg!(max);
+    let mut min = 2;
+    //dbg!(min);
+    for i in min..=max{
+        let dsum = helper::digits_iterator(i).map(|d| powers[d]).sum::<usize>();
+        //dbg!(i,dsum);
+        if dsum == i{
+            sum += i;
+        }
+    }
+    sum
+}
