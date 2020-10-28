93bc37d745c372c7323b1e64f95e24ffba6fd1bc
Simon Auch <auchsimon@googlemail.com>
2020-10-28 15:24:07 +0100

    improved check bench

--- src/helper.rs
+++ src/helper.rs
@@ -94,18 +94,20 @@ pub fn digit_sum(mut n: usize) -> usize {
 }
 
 #[inline(always)]
-pub fn time_it<F: Fn() -> ()>(f: F, loops: usize) {
+pub fn time_it<F: Fn() -> ()>(f: F, loops: usize) -> f64 {
     use std::time::Instant;
     let now = Instant::now();
     for _ in 0..loops {
         f();
     }
-    dbg!(now.elapsed().as_secs_f64() / loops as f64);
+    let total = now.elapsed().as_secs_f64();
+    dbg!(total / loops as f64);
+    total
 }
 
 pub fn check_bench<F: Fn() -> ()>(f: F) {
-    time_it(|| f(), 1);
-    time_it(|| f(), 10);
-    time_it(|| f(), 100);
-    time_it(|| f(), 1000);
+    let mut loops = 1;
+    while loops < 100_000_000 && time_it(|| f(), loops) < 1.0 {
+        loops *= 10;
+    }
 }
