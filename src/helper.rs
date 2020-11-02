ae68e23a1026099468a44685367288c5bca3dc29
Simon Auch <auchsimon@googlemail.com>
2020-11-02 14:33:08 +0100

    added function to read a single usize from stdin

--- src/helper.rs
+++ src/helper.rs
@@ -10,6 +10,17 @@ pub fn time_it<F: Fn() -> ()>(f: F, loops: usize) -> f64 {
     total
 }
 
+pub fn get_usize() -> usize {
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .expect("IO Error while reading from stdin.");
+    buffer
+        .trim()
+        .parse::<usize>()
+        .expect("Could not parse input as usize")
+}
+
 pub fn check_bench<F: Fn() -> ()>(f: F) {
     let mut loops = 1;
     while loops < 100_000_000 && time_it(|| f(), loops) < 1.0 {
